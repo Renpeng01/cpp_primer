6e75dda1c5782daa8b6ce2f12e4a87882ad17408
renpeng <renpeng@p1.com>
2023-07-02 10:46:13 +0800

    add

---
+++ src/3.cc
@@ -0,0 +1,43 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+int main0() {
+  std::string s;
+
+  // 在执行读取操作时，string对象会自动忽略开头的空白（空格、制表符。换行符）
+  // 并从第一个真正的字符开始读起，直到遇到限一次空白
+  std::cin >> s;
+  std::cout << "renpeng" << s << std::endl;
+  return 0;
+}
+
+int main1() {
+  std::string s;
+
+  // while (std::cin >> s) { //
+  // 如果流有效，也就是没有遇到文件结束标记或者非法输入
+  //   std::cout << "renpeng " << s << std::endl;
+  // }
+
+  while (getline(
+      std::cin,
+      s)) { // getline 直到遇到换行符为止 （换行符也会被读进来 但是s不存换行符）
+    std::cout << "renpeng " << s << std::endl;
+  }
+  return 0;
+}
+
+int main() {
+  std::string s("Hello World!!!!");
+  decltype(s.size()) punct_cnt = 0;
+
+  for (auto c : s) {
+    if (std::ispunct(c)) {
+      ++punct_cnt;
+    }
+  }
+
+  std::cout << punct_cnt << std::endl;
+  return 0;
+}
