d66cc85c3c7ddbaa69ce2de1b6ab79e7597d91ec
renpeng <renpeng@hellogroup.com>
2023-11-03 14:03:15 +0800

    add

---
+++ src/3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main1() {
+  std::string s;
+  std::cin >> s;
+
+  std::cout << s << std::endl;
+  return 0;
+}
+
+int main() {
+  std::string s1, s2;
+  std::cin >> s1 >> s2;
+
+  std::cout << s1 << s2 << std::endl;
+  return 0;
+}
